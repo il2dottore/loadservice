ae44fd5616b091abc6effbd67f99c4a5d9ec43a5
il2dottore <hovatenlavnb@gmail.com>
2026-07-16 19:33:26 +0700

    fix: init db

--- backend/scripts/drop-db.ts
+++ backend/scripts/drop-db.ts
@@ -4,6 +4,8 @@ const debugCoreDb = debugDb(process.env.CORE_SERVICE_DB ?? 'core_service_db');
 
 const debugAttackDb = debugDb(process.env.ATTACK_SERVICE_DB ?? 'attack_service_db');
 
+const debugPaymentDb = debugDb(process.env.ATTACK_SERVICE_DB ?? 'payment_service_db');
+
 async function main() {
 
   await debugCoreDb.execute(`DROP SCHEMA public CASCADE;`);
@@ -13,6 +15,10 @@ async function main() {
   await debugAttackDb.execute(`DROP SCHEMA public CASCADE;`);
   await debugAttackDb.execute(`CREATE SCHEMA public;`);
   await debugAttackDb.$client.end();
+
+  await debugPaymentDb.execute(`DROP SCHEMA public CASCADE;`);
+  await debugPaymentDb.execute(`CREATE SCHEMA public;`);
+  await debugPaymentDb.$client.end();
 }
 
 main();
