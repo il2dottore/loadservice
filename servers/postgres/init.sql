6b773ab6c6091b24e393f3e39f8314e1a5403083
il2dottore <hovatenlavnb@gmail.com>
2026-07-16 19:36:06 +0700

    chore: remove old config

--- servers/postgres/init.sql
+++ servers/postgres/init.sql
@@ -1,9 +1,3 @@
 CREATE DATABASE core_service_db;
-GRANT ALL PRIVILEGES ON DATABASE core_service_db TO sussybaka;
-
 CREATE DATABASE attack_service_db;
-GRANT ALL PRIVILEGES ON DATABASE attack_service_db TO sussybaka;
-
 CREATE DATABASE payment_service_db;
-GRANT ALL PRIVILEGES ON DATABASE payment_service_db TO sussybaka;
-
