fe1becce97ad5409034d3e5e90276e7266ec11ae
il2dottore <hovatenlavnb@gmail.com>
2026-07-18 12:20:50 +0700

    ci: only release when push tags

--- attack-node-service/utils/utils.go
+++ attack-node-service/utils/utils.go
@@ -2,16 +2,8 @@ package utils
 
 import (
 	"os/exec"
-	"runtime"
 )
 
 func ShellExec(shellCommand string) error {
-	osName := runtime.GOOS
-	switch osName {
-	case "windows":
-		return exec.Command("cmd", "/C", shellCommand).Run()
-	case "linux":
-		return exec.Command("/bin/sh", "-c", shellCommand).Run()
-	}
-	return nil
+	return exec.Command("/bin/sh", "-c", shellCommand).Run()
 }
